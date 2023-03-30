c3a6edf6ced67c059ca2e9f8f61ff44eb0893235
patiencewanjiru <patiencewanjiru123@gmail.com>
2023-03-30 22:05:23 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,235 +1,140 @@
-window.addEventListener('DOMContentLoaded', ()=>{
-    const cuteBtn = document.getElementById('cute')
-    const monkeyBtn = document.getElementById('monkey')
-    const zebraBtn = document.getElementById('zebra')
-    const lionBtn = document.getElementById('lion')
-    const pandaBtn = document.getElementById('panda')
-    
-    // Displaying a cat gif inside the DOM//
-    cuteBtn.addEventListener('click', ()=>{
-        const fetchCute = fetch('/db.json')
-        fetchCute.then(res => {return res.json()}).then(data =>{renderCute(data.characters)})
-    
-        //rendering the image inside the DOM//
-        function renderCute(data){
-            const cute = document.querySelector('#cute-path')
-            const cuteImg = document.createElement('img')
-            cuteImg.src = data[0].image
-            cuteImg.alt = 'cute pic'
+//Linking the JSON server with Javascript
+const BASE_URL = "http://localhost:3000";
+const headers = {
+    'Content-Type': 'application/json',
+};
 
-            // creating a div that houses the button //
-            const voteSection = document.createElement('div')
-            voteSection.style.display = 'flex'
-            voteSection.style.flexDirection = 'row-reverse'
-    
+document.addEventListener('DOMContentLoaded', (e) => {
+    e.preventDefault();
+    fetchCharacters();
+});
 
-            // creating a div that houses the button //
-            let voteNumber = document.createElement('h4')
-            voteNumber.style.marginLeft = '6px'
-            voteNumber.textContent = data[0].votes 
-    
-            voteSection.appendChild(voteNumber)
-    
-             // vote button inside the idSections div//
-            const voteBtn = document.createElement('button')
-            voteBtn.style.width = '85px'
-            voteBtn.style.background = 'white'
-            voteBtn.style.color = 'blue'
-            voteBtn.style.border = '2px solid blue'
-            voteBtn.textContent = 'Vote for cat'
-    
-            voteSection.appendChild(voteBtn)
-    
-            cute.appendChild(cuteImg)
-            cute.appendChild(voteSection)
 
-             // When the voteBtn is pressed it should increase the no. of votes for monkey//
-            voteBtn.addEventListener('click', ()=>{
-                voteNumber.textContent = data[0].votes++
-            })
+//Fetch characters from db.json then render them or display on our Html
+function fetchCharacters() {
+    fetch(`${BASE_URL}/characters`, {
+        method: 'GET' ,
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    })
+    .then((res) => res.json())
+    .then(renderCharacters)
+    .catch((err) => {
+        console.log(err);
+    });
+}
+
+
+//Render characters on the Html or Attach characters
+function renderCharacters(characters) {
+    const listCharactersDiv = document.getElementById('list-characters');
+    characters.forEach((character) => {
+        //Create a new div element for each character to be used
+        const card = document.createElement('div');
+        card.innerText = character.name;
+        card.classList.add('list-character-item');
+
+        //Add an eventlistener(onclick listener)
+        card.addEventListener('click',() => {
+            renderCharactersDetails(character);
+        });
+
+        //Append each new card to the listCharacterDiv
+        listCharactersDiv.appendChild(card);
+    });
+}
+
+
+//Render ONE character on the Html
+async function renderCharactersDetails(passedCharacters) {
+    const response = await fetch(
+        `${BASE_URL}/characters/${passedCharacters.id}`,
+        {
+            method: 'GET',
+            headers,
         }
+    );
+    const character = await response.json();
+    const characterDetailsDiv = document.getElementById('character-details');
+
+  //Reset character details div
+  characterDetailsDiv.innerHTML = '';
+
+  //Name the element
+  const nameParagraph = document.createElement('p');
+  nameParagraph.innerText = `Name: ${character.name}`;
+
+  //Image of the element
+  const imageElement = document.createElement('img');
+  imageElement.src = character.image;
+  imageElement.classList.add('character.image');
+
+  //Votes for the element
+  const voteParagrapgh = document.createElement('p');
+  voteParagrapgh.innerText = `Votes ${character.votes}`;
+
+  //Add Votes Button
+  const addVotesButton = document.createElement('button');
+  addVotesButton.innerText = 'Add votes';
+  addVotesButton.type = 'button';
+
+  //To prevent errors from occuring when pressing vote button
+  addVotesButton.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    //Add ONE vote to existing votes
+    character.votes = character.votes += 1;
+
+    //Outputs updates of character votes in JSON when vote button is pressed
+    updateCharactersVotes(character);
+  });
+
+
+//Reset votes button thus when pressed returns to 0
+const resetVotesButton = document.createElement('button');
+resetVotesButton.innerText = 'Reset Votes';
+resetVotesButton.addEventListener('click', (e) => {
+    e.preventDefault();
+    character.votes = 0;
+    resetCharactresVotes(character);
+});
+
+
+//Attach ALL elements
+characterDetailsDiv.append(
+    nameParagraph,
+    imageElement,
+    voteParagrapgh,
+    addVotesButton,
+    resetVotesButton
+);
+}
+
+
+//Update the votes of ONE character in JSON
+function updateCharactersVotes(character) {
+    fetch(`${BASE_URL}/characters/${character.id}`, {
+        method: 'PUT',
+        headers,
+        body: JSON.stringify(character),
     })
-    
-     // Displaying a zebra gif inside the DOM//
-    monkeyBtn.addEventListener('click', ()=>{
-        const fetchCute = fetch('/db.json')
-        fetchCute.then(res => {return res.json()}).then(data =>{renderMonkey(data.characters)})
-    
-        function renderMonkey(data){
-            // rendering the image inside the DOM//
-            const monkey = document.querySelector('#monkey-path')
-            const monkeyImg = document.createElement('img')
-            monkeyImg.src = data[1].image
-            monkeyImg.alt = 'monkey pic'
-    
-            // creating a div that houses the button //
-            const voteSection = document.createElement('div')
-            voteSection.style.display = 'flex'
-            voteSection.style.flexDirection = 'row-reverse'
-    
-    
-            // creating votemonkey inside the idSection div//
-            const votemonkey = document.createElement('h4')
-            votemonkey.style.marginLeft = '6px'
-            votemonkey.textContent = 0
-    
-            voteSection.appendChild(votemonkey)
-    
-            // vote button inside the idSections div//
-            const voteBtn = document.createElement('button')
-            voteBtn.style.width = '85px'
-            voteBtn.style.background = 'white'
-            voteBtn.style.color = 'blue'
-            voteBtn.style.border = '2px solid blue'
-            voteBtn.textContent = 'Vote for monkey'
-    
-            // When the voteBtn is pressed it should increase the no. of votes for monkey//
-            voteBtn.addEventListener('click', ()=>{
-                votemonkey.textContent = data[1].votes++
-            })
-            voteSection.appendChild(voteBtn)
-    
-            monkey.appendChild(monkeyImg)
-            monkey.appendChild(voteSection)
-        }
+    .then((res) => res.json())
+    .then((character) => {
+        renderCharactersDetails(character);
     })
-    
-    
-    
-    // Displaying a zebra gif inside the DOM//
-    zebraBtn.addEventListener('click', ()=>{
-        const fetchCute = fetch('/db.json')
-        fetchCute.then(res => {return res.json()}).then(data =>{renderzebra(data.characters)})
-    
-        function renderzebra(data){
-            // rendering the image inside the DOM//
-            const zebra = document.querySelector('#zebra-path')
-            const zebraImg = document.createElement('img')
-            zebraImg.src = data[2].image
-            zebraImg.alt = 'zebra pic'
-    
-            // creating a div that houses the button //
-            const voteSection = document.createElement('div')
-            voteSection.style.display = 'flex'
-            voteSection.style.flexDirection = 'row-reverse'
-    
-    
-            // creating voteZebra inside the idSection div//
-            const voteZebra = document.createElement('h4')
-            voteZebra.style.marginLeft = '6px'
-            voteZebra.textContent = 0
-    
-            voteSection.appendChild(voteZebra)
-    
-            // vote button being displayed inside the idSections div//
-            const voteBtn = document.createElement('button')
-            voteBtn.style.width = '85px'
-            voteBtn.style.background = 'white'
-            voteBtn.style.color = 'blue'
-            voteBtn.style.border = '2px solid blue'
-            voteBtn.textContent = 'Vote for zebra'
-    
-            // When the voteBtn is pressed it should increase the no. of votes of zebras//
-            voteBtn.addEventListener('click', ()=>{
-                voteZebra.textContent = data[2].votes++
-            })
-            voteSection.appendChild(voteBtn)
-    
-            zebra.appendChild(zebraImg)
-            zebra.appendChild(voteSection)
-        }
+    .catch((err) => console.log(err));
+}
+
+
+//Reset the votes of ONE character
+function resetCharactresVotes(character) {
+    fetch(`${BASE_URL}/characters/${character.id}`, {
+        method:'PUT',
+        headers,
+        body: JSON.stringify(character),
     })
-    
-    
-    // Displaying a lion gif inside the DOM//
-    lionBtn.addEventListener('click', ()=>{
-        const fetchCute = fetch('/db.json')
-        fetchCute.then(res => {return res.json()}).then(data =>{renderlion(data.characters)})
-    
-        function renderlion(data){
-            // rendering the image inside the DOM//
-            const lion = document.querySelector('#lion-path')
-            const lionImg = document.createElement('img')
-            lionImg.src = data[3].image
-            lionImg.alt = 'lion pic'
-    
-            // creating a div that houses the button //
-            const voteSection = document.createElement('div')
-            voteSection.style.display = 'flex'
-            voteSection.style.flexDirection = 'row-reverse'
-    
-    
-            // creating voteLion inside the idSection div//
-            const voteLion = document.createElement('h4')
-            voteLion.style.marginLeft = '6px'
-            voteLion.textContent = 0
-    
-            voteSection.appendChild(voteLion)
-    
-            // vote button inside the idSections div
-            const voteBtn = document.createElement('button')
-            voteBtn.style.width = '85px'
-            voteBtn.style.background = 'white'
-            voteBtn.style.color = 'blue'
-            voteBtn.style.border = '2px solid blue'
-            voteBtn.textContent = 'Vote for lion'
-    
-            // When the voteBtn is pressed it should increase the no. of votes of lions//
-            voteBtn.addEventListener('click', ()=>{
-                voteLion.textContent = data[3].votes++
-            })
-            voteSection.appendChild(voteBtn)
-    
-            lion.appendChild(lionImg)
-            lion.appendChild(voteSection)
-        }
-    })
-    
-    
-    // Displaying a panda gif inside the DOM//
-    pandaBtn.addEventListener('click', ()=>{
-        const fetchCute = fetch('/db.json')
-        fetchCute.then(res => {return res.json()}).then(data =>{renderpanda(data.characters)})
-    
-        function renderpanda(data){
-            // rendering the image inside the DOM//
-            const panda = document.querySelector('#panda-path')
-            const pandaImg = document.createElement('img')
-            pandaImg.src = data[4].image
-            pandaImg.alt = 'panda pic'
-    
-            // creating a div that houses the button  //
-            const voteSection = document.createElement('div')
-            voteSection.style.display = 'flex'
-            voteSection.style.flexDirection = 'row-reverse'
-    
-    
-            // creating votePanda inside the idSection div//
-            const votePanda = document.createElement('h4')
-            votePanda.style.marginLeft = '6px'
-            votePanda.textContent = 0
-    
-            voteSection.appendChild(votePanda)
-    
-            // vote button inside the idSections div//
-            const voteBtn = document.createElement('button')
-            voteBtn.style.width = '85px'
-            voteBtn.style.background = 'white'
-            voteBtn.style.color = 'blue'
-            voteBtn.style.border = '2px solid blue'
-            voteBtn.textContent = 'Vote for panda'
-    
-            // When the voteBtn is pressed it should increase the no. of votes of panda//
-            voteBtn.addEventListener('click', ()=>{
-                votePanda.textContent = data[4].votes++
-            })
-            voteSection.appendChild(voteBtn)
-    
-            panda.appendChild(pandaImg)
-            panda.appendChild(voteSection)
-        }
-    })
-    
-    })
-    
-    
+    .then((res) => res.json())
+        .then((character) => renderCharactersDetails(character))
+        .catch((err) => console.log(err));
+}
